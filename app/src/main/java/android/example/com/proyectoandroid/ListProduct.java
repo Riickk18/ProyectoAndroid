6c22d8710f3810ae7a70ae8a5836bb4e88fbee6e
Riickk18 <richard_orlan@hotmail.com>
2018-05-13 17:21:45 -0400

    Agrego la ListProduct

--- app/src/main/java/android/example/com/proyectoandroid/ListProduct.java
+++ app/src/main/java/android/example/com/proyectoandroid/ListProduct.java
@@ -1,4 +1,13 @@
 package android.example.com.proyectoandroid;
 
-public class ListProduct {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_product);
+    }
 }
